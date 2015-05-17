51299a304efd9d9dc450084a4aef1cc8b0ec6834
Cory Diamand <corydiamand@gmail.com>
2015-05-17 16:08:21 -0400

    homepage spacing adjustment. 45 word paragraph evenly divided with 3 sentences

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,10 +1,14 @@
 class HomeController < ApplicationController
   def index
     #Eventually break up into sentences with punctuation and proper capitalization
-    @terms = TermSet.terms.shuffle[0..60].each do |term|
+    @terms = TermSet.terms.shuffle[0..45].each do |term|
       term.downcase!
     end
     @terms.first.capitalize!
+    @terms[16].capitalize!
+    @terms[31].capitalize!
+    @terms[15].concat('.')
+    @terms[30].concat('.')
     @terms.last.concat('.')
     @final_terms = @terms.join(" ")
   end
